851c576e4848d203255ff3dc9ed506a06f5ca807
Alvin <alvinchee96@gmail.com>
2020-08-15 10:17:50 +0800

    fix: Reformat code

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -1,12 +1,12 @@
-import {scaleFont} from './mixins';
+import {scaleFont} from "./mixins";
 
 // FONT FAMILY
-export const FONT_FAMILY_REGULAR = 'OpenSans-Regular';
-export const FONT_FAMILY_BOLD = 'OpenSans-Bold';
+export const FONT_FAMILY_REGULAR = "OpenSans-Regular";
+export const FONT_FAMILY_BOLD = "OpenSans-Bold";
 
 // FONT WEIGHT
-export const FONT_WEIGHT_REGULAR = '400';
-export const FONT_WEIGHT_BOLD = '700';
+export const FONT_WEIGHT_REGULAR = "400";
+export const FONT_WEIGHT_BOLD = "700";
 
 // FONT SIZE
 export const FONT_SIZE_16 = scaleFont(16);
@@ -20,11 +20,11 @@ export const LINE_HEIGHT_16 = scaleFont(16);
 
 // FONT STYLE
 export const FONT_REGULAR = {
-  fontFamily: FONT_FAMILY_REGULAR,
-  fontWeight: FONT_WEIGHT_REGULAR,
+    fontFamily: FONT_FAMILY_REGULAR,
+    fontWeight: FONT_WEIGHT_REGULAR
 };
 
 export const FONT_BOLD = {
-  fontFamily: FONT_FAMILY_BOLD,
-  fontWeight: FONT_WEIGHT_BOLD,
+    fontFamily: FONT_FAMILY_BOLD,
+    fontWeight: FONT_WEIGHT_BOLD
 };
